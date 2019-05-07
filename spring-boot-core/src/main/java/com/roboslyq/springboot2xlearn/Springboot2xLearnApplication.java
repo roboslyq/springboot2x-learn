192df1880698685c16888b97674e7c42c54c137f
roboslyq <robosloveu@163.com>
2019-05-07 16:57:54 +0800

    updated

--- spring-boot-core/src/main/java/com/roboslyq/springboot2xlearn/Springboot2xLearnApplication.java
+++ spring-boot-core/src/main/java/com/roboslyq/springboot2xlearn/Springboot2xLearnApplication.java
@@ -1,6 +1,5 @@
 package com.roboslyq.springboot2xlearn;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
