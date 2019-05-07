3b25816b0b080276af9611e058c83c4f228bb4e4
roboslyq <robosloveu@163.com>
2019-05-08 01:20:04 +0800

    spring mvc webflux exception

--- spring-webflux/src/main/java/com/roboslyq/webflux/exception/GlobalErrorWebExceptionHandler.java
+++ spring-webflux/src/main/java/com/roboslyq/webflux/exception/GlobalErrorWebExceptionHandler.java
@@ -1,39 +1,44 @@
 package com.roboslyq.webflux.exception;
-
 import com.roboslyq.webflux.exception.util.JsonHttpMessageWriter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ResourceProperties;
 import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
 import org.springframework.boot.web.reactive.error.ErrorAttributes;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.HttpMessageWriter;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Mono;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+@Component
+@Order(-3)
+public class GlobalErrorWebExceptionHandler extends AbstractErrorWebExceptionHandler  {
 
-/**
- *
- * @author itning
- * @date 2019/4/29 18:13
- */
-//@Component
-//@Order(-4)
-public class ErrorWebExceptionHandlerDemo extends AbstractErrorWebExceptionHandler {
-    public ErrorWebExceptionHandlerDemo(JsonHttpMessageWriter jsonHttpMessageWriter,
-                                          ErrorAttributes errorAttributes,
-                                          ResourceProperties resourceProperties,
-                                          ApplicationContext applicationContext) {
+    @Autowired
+    JsonHttpMessageWriter jsonHttpMessageWriter;
+    public GlobalErrorWebExceptionHandler(JsonHttpMessageWriter jsonHttpMessageWriter,
+                                        ErrorAttributes errorAttributes,
+                                        ResourceProperties resourceProperties,
+                                        ApplicationContext applicationContext) {
         super(errorAttributes, resourceProperties, applicationContext);
+
+    }
+    @Override
+    public void afterPropertiesSet() throws Exception {
         setMessageWriters(Collections.singletonList(jsonHttpMessageWriter));
     }
 
     @Override
     protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
-        System.out.println("ErrorWebExceptionHandlerDemo... ...");
+        System.out.println("GlobalWebExceptionHandlerDemo... ...");
         return RouterFunctions.route(
                 RequestPredicates.all(), this::renderErrorResponse);
     }
